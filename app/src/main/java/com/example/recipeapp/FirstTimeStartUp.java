4642677c6fb09fe1d3430a0d90902909ca68a8fd
Michael <michael.chau0424@gmail.com>
2022-03-10 22:09:23 +1100

    first commit

---
+++ app/src/main/java/com/example/recipeapp/FirstTimeStartUp.java
@@ -0,0 +1,28 @@
+package com.example.recipeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstTimeStartUp extends AppCompatActivity {
+    Button buttonContinue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_time_start_up);
+
+        buttonContinue = findViewById(R.id.button);
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FirstTimeStartUp.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
