df350980061a6e1fc2d321de9f65165bd47ef5b2
Michael <michael.chau0424@gmail.com>
2022-03-11 13:43:51 +1100

    added new activities and linked together

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -3,15 +3,38 @@ package com.example.recipeapp;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    Button buttonCapture;
+    Button buttonSavedContent;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCapture = findViewById(R.id.buttonCapture);
+        buttonSavedContent = findViewById(R.id.buttonSavedContent);
+
+        buttonCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Capture.class);
+                startActivity(intent);
+
+            }
+        });
+
+        buttonSavedContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SavedContent.class);
+                startActivity(intent);
+            }
+        });
     }
 }
